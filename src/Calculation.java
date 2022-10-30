911c7455046fcc2ce8e9ec267f5f62010260ae36
MarcinLes7 <Gitm@rtino7>
2022-10-30 10:23:57 +0100

    Dodanie komentarza do programu

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,5 +1,6 @@
 public class Calculation {
     public static void main(String[] args) {
+        // to jest prosta pętla służąca do wylistowania cyfr od 0 do 100
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
         }
